955b73ea2c2d791410e17283ed3d264bc85f7789
Dev Jana <dev@primeacademy.io>
2018-11-09 10:31:56 -0600

    basic interface

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,15 @@
-console.log( 'js' );
+$( document ).ready( readyNow );
+
+function addItem(){
+    console.log( 'in addItem' );
+} // end addItem
+
+function filterList(){
+    console.log( 'in filterList' );
+} // end filterList
+
+function readyNow(){
+    console.log( 'in readyNow' );
+    $( '#addItemButton' ).on( 'click', addItem );
+    $( '#filterListButton' ).on( 'click', filterList );
+} // readyNow
