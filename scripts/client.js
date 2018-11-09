9db5a9ab0f7bf00ce65927ab641d704aef8a72aa
Dev Jana <dev@primeacademy.io>
2018-11-09 11:50:59 -0600

    end of lecture

--- scripts/client.js
+++ scripts/client.js
@@ -31,28 +31,60 @@ function addItem(){
         // empty inputs
         $( '#itemNameIn' ).val( '' );
         $( '#itemDescriptionIn' ).val( '' );
-        updateInventory();
+        updateInventory( inventory );
     } // no empties
 } // end addItem
 
+function displayAdd(){
+    console.log( 'in displayAdd' );
+    $( '#inputDiv' ).show();
+    $( '#outputDiv' ).hide();
+} // end displayAdd
+
+function displaySearch(){
+    console.log( 'in displaySearch' );
+    $( '#inputDiv' ).hide();
+    $( '#outputDiv' ).show();
+} // end displaySearch
+
 function filterList(){
     console.log( 'in filterList' );
+    let matches = [];
+    // get two filter fields (size/color)
+    // loop through the inventory and collect matches
+    for( item of inventory ){
+        if( item.size === $( '#searchSizeIn').val() && item.color === $( '#searchColorIn').val() ){
+            // found match
+            matches.push( item );
+        } // end match
+    } // end for of
+    updateInventory( matches );
+    console.log( 'matches:', matches );
 } // end filterList
 
 function readyNow(){
     console.log( 'in readyNow' );
     $( '#addItemButton' ).on( 'click', addItem );
     $( '#filterListButton' ).on( 'click', filterList );
+    $( '#allButton' ).on( 'click', showAll );
+    $( '#addNewButton' ).on( 'click', displayAdd );
+    $( '#viewSearchButton' ).on( 'click', displaySearch );
+    displayAdd();
 } // readyNow
 
-function updateInventory(){
+function showAll(){
+    console.log( 'in showAll' );
+    updateInventory( inventory );
+} // end showAll
+
+function updateInventory( array ){
     console.log( 'in updateInventory' );
     // target the ul by ID
     let el =  $( '#itemList' );
     // empty ul
     el.empty();
     // loop through the inventory & display each item in ul
-    for( let item of inventory ){
+    for( let item of array ){
         let displayString = `<li>${item.name}, ${item.description}: ${item.size} ${item.color}</li>`;
         el.append( displayString );
     } // end for 
